8312ac4f79609b22a621b40cb03a8ab05586dce3
Costa Monzili <costa.monzili@adevinta.com>
2023-08-21 17:14:45 +0200

    Initial Commit

---
+++ EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Costa Monzili on 21/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
