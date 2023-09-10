e8a4259062cb6976eb73b3091efdbb66fa1023de
Costa Monzili <costa.monzili@adevinta.com>
2023-09-10 19:58:56 +0200

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Costa Monzili on 10/09/2023.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+}
