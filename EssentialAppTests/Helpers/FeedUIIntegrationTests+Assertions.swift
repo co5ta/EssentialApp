86c6e3f4f6c67a53ae4867903d1b4e79ca6146eb
Costa Monzili <costa.monzili@adevinta.com>
2023-09-24 00:15:06 +0200

    Execute RunLoop to prevent memory leaks in the test (On iOS 14+, you may need to execute the RunLoop one more time after asserting the UI state to reliably prevent the test leak)

--- EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
+++ EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
@@ -31,5 +31,10 @@ extension FeedUIIntegrationTests {
         feed.enumerated().forEach { index, image in
             assertThat(sut, hasViewConfiguredFor: image, at: index)
         }
+        executeRunLoopToCleanUpReferences()
+    }
+
+    private func executeRunLoopToCleanUpReferences() {
+        RunLoop.current.run(until: Date())
     }
 }
