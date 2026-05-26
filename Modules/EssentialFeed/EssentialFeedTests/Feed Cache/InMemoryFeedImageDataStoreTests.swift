78c3237b263f3a42ff16246d493359db3ea14200
Costa Monzili <costa.monzili@adevinta.com>
2026-05-26 10:51:32 +0200

    Promote InMemoryFeedStore to production
    Replaced Dictionary with NSCache to support automatic eviction when resources are low.

---
+++ Modules/EssentialFeed/EssentialFeedTests/Feed Cache/InMemoryFeedImageDataStoreTests.swift
@@ -0,0 +1,45 @@
+//
+//  InMemoryFeedImageDataStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Costa Monzili on 26/05/2026.
+//
+
+import XCTest
+import EssentialFeed
+
+class InMemoryFeedImageDataStoreTests: XCTestCase, FeedImageDataStoreSpecs {
+
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversNotFoundOnEmptyCache(on: sut)
+    }
+
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversNotFoundWhenStoredDataURLDoesNotMatch(on: sut)
+    }
+
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversFoundDataWhenThereIsAStoredImageDataMatchingURL(on: sut)
+    }
+
+    func test_retrieveImageData_deliversLastInsertedValue() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversLastInsertedValueForURL(on: sut)
+    }
+
+    // - MARK: Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> InMemoryFeedStore {
+        let sut = InMemoryFeedStore()
+        trackForMemoryLeaks(element: sut, file: file, line: line)
+        return sut
+    }
+
+}
